e91379d2eee1abb62a7ef0cd5150dbe25171ac71
Tiago Buriol <tiagoburiol@gmail.com>
2019-08-16 17:38:49 -0300

    primeiro teste estragando

--- CPIOII/laplace1.py
+++ CPIOII/laplace1.py
@@ -25,7 +25,9 @@ dic = {'red': ((0., 1, 1), (0.00000000001, 0, 0), (0.66, 1, 1), (0.89,1, 1), (1,
        'green': ((0., 1, 1), (0.00000000001, 0, 0), (0.375,1, 1), (0.64,1, 1), (0.91,0,0), (1, 0, 0)), 
        'blue': ((0., 1, 1), (0.00000000001, 1, 1), (0.34, 1, 1), (0.65,0, 0), (1, 0, 0))}
 my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap',dic,256)
-
+oioaghdfbnfgapuisdfhgpiaudhfgviu
+açskdjfbasdiufpiausdbf
+alidbfsdubfçuba
 # plot
 pcolor(T,cmap=my_cmap)
 colorbar()
